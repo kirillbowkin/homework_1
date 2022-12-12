c5d2b94dfeceb97821b64c74145f4e23eece1cb3
kirillbowkin <kirillshishkin77@gmail.com>
2022-12-12 15:28:06 +0300

    3. Чтение данных с помощью DataManager

---
+++ src/main/java/com/sample/carmarket/app/service/CarsService.java
@@ -0,0 +1,35 @@
+package com.sample.carmarket.app.service;
+
+import com.sample.carmarket.entity.EngineType;
+import io.jmix.core.DataManager;
+import io.jmix.core.entity.KeyValueEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+@Service
+public class CarsService {
+
+    @Autowired
+    private DataManager dataManager;
+
+    public Map<EngineType, Long> calculateCars(UUID manufacturerId) {
+        List<KeyValueEntity> values = dataManager.loadValues("" +
+                        "select c.model.engineType, count(c.model.engineType) " +
+                        "from Car c where c.model.manufacturer.id = :manufacturerId " +
+                        "group by c.model.engineType")
+                .parameter("manufacturerId", manufacturerId)
+                .properties("engineType", "count")
+                .list();
+
+        return values.stream()
+                .collect(Collectors.toMap(
+                        kv -> EngineType.fromId(kv.getValue("engineType")),
+                        kv -> kv.<Long>getValue("count")
+                ));
+    }
+}
--- src/main/java/com/sample/carmarket/screen/manufacturer/ManufacturerBrowse.java
+++ src/main/java/com/sample/carmarket/screen/manufacturer/ManufacturerBrowse.java
@@ -1,10 +1,43 @@
 package com.sample.carmarket.screen.manufacturer;
 
-import io.jmix.ui.screen.*;
+import com.sample.carmarket.app.service.CarsService;
+import com.sample.carmarket.entity.EngineType;
 import com.sample.carmarket.entity.Manufacturer;
+import io.jmix.ui.Notifications;
+import io.jmix.ui.action.Action;
+import io.jmix.ui.component.Table;
+import io.jmix.ui.screen.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.Map;
+import java.util.Objects;
 
 @UiController("Manufacturer.browse")
 @UiDescriptor("manufacturer-browse.xml")
 @LookupComponent("table")
 public class ManufacturerBrowse extends MasterDetailScreen<Manufacturer> {
+    @Autowired
+    private Notifications notifications;
+    @Autowired
+    private Table<Manufacturer> table;
+    @Autowired
+    private CarsService carsService;
+
+    @Subscribe("table.calculateCars")
+    public void onTableCalculateCars(Action.ActionPerformedEvent event) {
+        Manufacturer selectedManufacturer = Objects.requireNonNull(table.getSingleSelected());
+
+        Map<EngineType, Long> engineTypesCount = carsService.calculateCars(selectedManufacturer.getId());
+
+        notifications.create()
+                .withCaption(
+                        "Electric cars = " + engineTypesCount.getOrDefault(EngineType.ELECTRIC, 0L) +
+                        ", gasoline cars = " + engineTypesCount.getOrDefault(EngineType.GASOLINE, 0L)
+                )
+                .withType(Notifications.NotificationType.TRAY)
+                .show();
+    }
+
 }
